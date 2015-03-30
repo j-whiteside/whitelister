cb6ad9461c7c53ed45263591e79e8cbd3d0d1a86
j-whiteside <whiteside.jeremy@gmail.com>
2015-03-30 15:57:36 -0400

    Restructuring - Out with interop, in with csvHelper

--- whitelister/Form1.cs
+++ whitelister/Form1.cs
@@ -8,22 +8,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
-//using Marshal = System.Runtime.InteropServices.Marshal;
+using System.Diagnostics;
+using CsvHelper;
 
 namespace whitelister
 {
     public partial class Form1 : Form
     {
         String path = (@"C:\exceltest\");
+        String modificationMacroPath = (@"C:\exceltest\formatData.xls");
         String filepath = (@"C:\exceltest\spam.csv");
         String filepathEdit = (@"C:\exceltest\spamEdit.csv");
-        private Excel.Application xlApp;
-        private Excel.Workbooks xlWorkBooks;
-        private Excel.Workbook xlWorkBook;
-        protected Excel.Sheets xlWorkSheets;
-        protected Excel.Worksheet xlWorkSheet;
-
 
         Boolean appFailed = false;
 
@@ -38,13 +33,14 @@ namespace whitelister
             fileLocation();
             if (appFailed != true)
             {
-                fileModification(path);
+                fileModification(modificationMacroPath);
             }
             else
             {
                 logger("Program terminating");
                 this.Close();
             }
+            spamLogParser(filepathEdit);
 
         }
 
@@ -55,9 +51,9 @@ namespace whitelister
 
             if (File.Exists(path + "spam.csv") == true)
             {
-                logger("File location successful!");
+                logger("Spam log location successful!");
                 File.Copy(filepath, filepathEdit, true);
-                logger("File duplication successful!");
+                logger("Spam log duplication successful!");
             }
             else
             {
@@ -66,100 +62,48 @@ namespace whitelister
             }
         }
 
-        
-        protected void fileModification(String path)
+        public void fileModification(String path)
         {
-            logger("File modification started");
-            
-            //xlWorkBook = xlApp.Workbooks.Open(filepathEdit, 0, false, 2, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, false, 1, 0);
-            xlApp = new Excel.Application();
-
-                
-            xlWorkBooks = xlApp.Workbooks;
-            xlWorkBook = xlWorkBooks.Open(filepathEdit, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-            xlWorkSheets = xlWorkBook.Worksheets;
-            xlWorkSheet = (Excel.Worksheet)xlWorkSheets[1];
-            xlWorkSheet.Select(Type.Missing);
-
-            int numberOfRowsInteger = File.ReadLines(@"C:\exceltest\spam.csv").Count();
-
-            //Delete all records older than a week
-
-            //xlWorkSheet.Range[G2, 7].EntireColumn.NumberFormat = "MM/dd/yyyy";
-
-            DateTime lastWeekDateTime = getLastWeek();
-
-            textBox1.Text = numberOfRowsInteger.ToString();
-                
-            /*
-                
-            for(int i = 1; i < numberOfRowsInteger ; i++)
-            {
-                if (xlWorkSheet.Cells[i,7] < (lastWeekDateTime))
-                {
-                    Excel.Range range = xlWorkSheet.get_Range(i, Type.Missing);
-                    range.Delete(Excel.XlDeleteShiftDirection.xlShiftUp);
-                    logger("Row" + i + " deleted");
-                }
-            }
-
-            /*
-            //Order by spam index
-            Excel.Range rngSort = xlWorkSheet.get_Range("A2", "J" + numberOfRowsInteger);
-
-            rngSort.Sort(rngSort.Columns[1, Type.Missing], Excel.XlSortOrder.xlAscending,
-                            rngSort.Columns[2, Type.Missing], Type.Missing, Excel.XlSortOrder.xlAscending,
-                            Type.Missing, Excel.XlSortOrder.xlAscending,
-                            Excel.XlYesNoGuess.xlYes, Type.Missing, Type.Missing,
-                            Excel.XlSortOrientation.xlSortColumns,
-                            Excel.XlSortMethod.xlPinYin,
-                            Excel.XlSortDataOption.xlSortNormal,
-                            Excel.XlSortDataOption.xlSortNormal,
-                            Excel.XlSortDataOption.xlSortNormal);
-            rngSort = null;
-
-            xlWorkBook.Save();
-                */
-
-            
-            xlWorkBook.Save();
-            xlWorkBook.Close(false, Type.Missing, Type.Missing);
-            xlApp.Quit();
-
-            releaseObject(xlWorkSheet);
-            releaseObject(xlWorkSheets);
-            releaseObject(xlWorkBook);
-            releaseObject(xlWorkBooks);
-            releaseObject(xlApp);
-            
+            logger("Formatting spam log file");
+            Process excel = new Process();
+            excel.StartInfo.FileName = path;
+            excel.Start();
+            excel.WaitForExit(20000);
+            logger("Spam log has been formatted");
         }
 
-        protected void releaseObject(object excelObject)
+        public void spamLogParser(String path)
         {
-            try
-            {
-                if (excelObject != null)
-                {
-                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelObject);
-                    excelObject = null;
-                }
+            StreamReader file = new StreamReader(path);
+            CsvReader csv = new CsvReader(file);
+            IEnumerable<DataRecord> records = csv.GetRecords<DataRecord>();
 
-            }
-            catch (Exception ex)
+            foreach (var rec in records) // Each record will be fetched and printed on the screen
             {
-                excelObject = null;
-                MessageBox.Show("Unable to release the Object " + ex.Message);
+                String spamScore = rec.name;
+                MessageBox.Show(spamScore);
+                
+                //Response.Write(string.Format("Name : {0}, Sex : {1}, Occupation : {2} <br/>", rec.name, rec.sex, rec.occupation));
             }
-            finally
-            {
-                GC.Collect();
-            }
+            
+            file.Close();
+            
+            
+            String sampleParse = csv.GetField(1);
+
+            logger(sampleParse);
+
+            //return recipient;
         }
 
-        public DateTime getLastWeek()
+
+        public class DataRecord // Test record class
         {
-            return DateTime.Today.AddDays(-7);
+            public string spamScore { get; set; }
+            public string Sender { get; set; }
+            public string occupation { get; set; }
         }
+
 
         public void emailer(String subject, String recipient, String body)
         {
@@ -168,7 +112,7 @@ namespace whitelister
 
         public void logger(String lines)
         {
-            using (System.IO.StreamWriter log = new System.IO.StreamWriter(path + "whitelistEmailerLog.txt", true))
+            using (StreamWriter log = new StreamWriter(path + "whitelistEmailerLog.txt", true))
             {
                 log.WriteLine(lines);
             }
